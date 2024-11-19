3047d8dab52bd2d752cb88371cda9aef24befdfc
Radamir <dermeister20032132134@gmail.com>
2024-11-19 17:44:41 +0300

    добавил stack.h и начал писать stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,17 @@
+//
+// Created by radamir on 19.11.24.
+//
+#include "stack.h"
+
+template<typename T>
+Stack<T>::Stack() {
+    head.data=0;
+    head.previous=nullptr;
+    curSize=0;
+}
+
+template<typename T>
+void Stack<T>::push(T data) {
+
+}
+
---
+++ stack.h
@@ -0,0 +1,28 @@
+//
+// Created by radamir on 19.11.24.
+//
+
+#ifndef STACK_H
+#define STACK_H
+template<typename  T>
+class Stack {
+private:
+    struct Node {
+        T data;
+        Node* previous;
+    };
+    int curSize;
+    Node head;
+public:
+    Stack();
+    void push(T data);
+    T top() const;
+    bool empty() const;
+    void pop();
+    int size() {
+
+    }
+
+
+};
+#endif //STACK_H
