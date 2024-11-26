bc25106dec84a043e4ad7c6b2cd87d64f4f80757
Radamir <dermeister20032132134@gmail.com>
2024-11-26 18:34:21 +0300

    добавил еще тестов

--- Google_tests/test1.cpp
+++ Google_tests/test1.cpp
@@ -53,3 +53,23 @@ TEST_F(CalculatorTest, HandlesInput7) {
     std::string expr = "sqrt9";
     EXPECT_EQ(3,calculatorDevice.calculate(expr));
 }
+TEST_F(CalculatorTest, HandlesInput8) {
+    std::string expr = "sqrt0";
+    EXPECT_EQ(0,calculatorDevice.calculate(expr));
+}
+TEST_F(CalculatorTest, HandlesInput9) {
+    std::string expr = "sqrt81";
+    EXPECT_EQ(9,calculatorDevice.calculate(expr));
+}
+TEST_F(CalculatorTest, HandlesInput10) {
+    std::string expr = "sqrt9*sin0";
+    EXPECT_EQ(0,calculatorDevice.calculate(expr));
+}
+TEST_F(CalculatorTest, HandlesInput11) {
+    std::string expr = "1+sqrt(18/2*2/3*3/2)+sin(0)";
+    EXPECT_EQ(4,calculatorDevice.calculate(expr));
+}
+TEST_F(CalculatorTest, HandlesInput12) {
+    std::string expr = "(0+2^2-4*sqrt(1))^2";
+    EXPECT_EQ(0,calculatorDevice.calculate(expr));
+}
